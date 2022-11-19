4f606313d6e08b9bb9d2be5a1e33d71de1285f79
Ponte-coder <jamateosb@gmail.com>
2022-11-19 21:24:47 +0100

    Fragment innecesario borrado.

--- app/src/main/java/es/unex/parsiapp/foldersActivity.java
+++ app/src/main/java/es/unex/parsiapp/foldersActivity.java
@@ -2,6 +2,7 @@ package es.unex.parsiapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,11 +21,13 @@ import es.unex.parsiapp.ui.gallery.folderContentFragment;
 
 public class foldersActivity extends AppCompatActivity {
     private List<Carpeta> listCarpeta;
-
+    private boolean bandera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.folders_activity);
+
+        bandera= true;
 
         AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
@@ -78,6 +81,7 @@ public class foldersActivity extends AppCompatActivity {
 
 
     public void moveToFolderContent(Carpeta item){
+        bandera= false;
         Fragment fragment = new folderContentFragment(item.getIdDb());
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -108,4 +112,15 @@ public class foldersActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        System.out.println(bandera+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        if (keyCode == event.KEYCODE_BACK && this.getClass() == foldersActivity.class && bandera) {
+            Intent intent = new Intent(this, MenuLateralActivity.class);
+            startActivity(intent);
+        }
+        bandera= true;
+        return super.onKeyDown(keyCode, event);
+    }
 }
