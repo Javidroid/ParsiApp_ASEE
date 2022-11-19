529d054d5f7f31a98cee7d6dd4045759963cc0da
Ponte-coder <jamateosb@gmail.com>
2022-11-19 12:13:08 +0100

    Diseño tweet detail(2).

---
+++ app/src/main/java/es/unex/parsiapp/tweetDetailsActivity.java
@@ -0,0 +1,14 @@
+package es.unex.parsiapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class tweetDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_details);
+    }
+}
