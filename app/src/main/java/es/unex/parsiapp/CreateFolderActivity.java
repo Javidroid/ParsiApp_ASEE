2c6d27c2e676be6131f5d09c2d313831e906459f
Pablo Rodríguez Martín <rodmarprogrammer@gmail.com>
2022-11-21 11:34:41 +0100

    Editar y crear columnas en un mismo Activity

--- app/src/main/java/es/unex/parsiapp/CreateFolderActivity.java
+++ app/src/main/java/es/unex/parsiapp/CreateFolderActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import es.unex.parsiapp.model.Carpeta;
@@ -13,17 +14,52 @@ import es.unex.parsiapp.roomdb.ParsiDatabase;
 
 public class CreateFolderActivity extends AppCompatActivity {
 
+    private Carpeta editedFolder = null;
+
     /* Metodos de Callback */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_folder);
+
+        /* Si createFolder es true, se creara una nueva carpeta. Si es false se editara una carpeta
+            ya existente y se cargaran los datos de esta en la IU */
+        boolean createFolder = true;
+        createFolder = getIntent().getBooleanExtra("create", createFolder);
+
+        // Si se esta editando una columna, se prepara la IU para cargar los datos
+        if (!createFolder){
+            long id_folder = 1;
+            id_folder = getIntent().getLongExtra("idfolder", id_folder);
+            setForEditFolder(id_folder);
+        }
     }
 
     @Override
     protected void onDestroy() {
         ParsiDatabase.getInstance(this).close();
         super.onDestroy();
+    }
+
+    public void setForEditFolder(long id_folder){
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                ParsiDatabase database = ParsiDatabase.getInstance(CreateFolderActivity.this);
+
+                editedFolder = database.getCarpetaDao().getFolder(id_folder);
+                AppExecutors.getInstance().mainThread().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        EditText fname = (EditText) findViewById(R.id.folderName);
+                        fname.setText(editedFolder.getNombre());
+
+                        Button b = (Button) findViewById(R.id.create_folder_button);
+                        b.setText("Editar");
+                    }
+                });
+            }
+        });
     }
 
     // Accion a realizar al pulsar el boton de crear tras establecer el nombre
@@ -34,7 +70,11 @@ public class CreateFolderActivity extends AppCompatActivity {
         // Si el nombre no esta vacio
         if(folderName.length() > 0) {
             // Crea la carpeta
-            createFolder(folderName);
+            if(editedFolder != null){
+                editFolder(folderName);
+            } else {
+                createFolder(folderName);
+            }
             Intent myIntent = new Intent(CreateFolderActivity.this, MenuLateralActivity.class);
             // Lanzar actividad MenuLateralActivity
             startActivity(myIntent);
@@ -53,6 +93,20 @@ public class CreateFolderActivity extends AppCompatActivity {
                 // Insercion en BD
                 long id = database.getCarpetaDao().insert(c);
                 c.setIdDb(id);
+            }
+        });
+    }
+
+    // Edita una carpeta (le asigna un nuevo nombre)
+    public void editFolder(String folderName){
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                // Declaracion de la instancia de la BD
+                ParsiDatabase database = ParsiDatabase.getInstance(CreateFolderActivity.this);
+                // Cambio de nombre y update en BD
+                editedFolder.setNombre(folderName);
+                database.getCarpetaDao().update(editedFolder);
             }
         });
     }
