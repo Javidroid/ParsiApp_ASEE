d40a23f49b0290312ef36cde863b32f5ca9d7f86
andresgl1306 <agarciavwl@alumnos.unex.es>
2022-12-07 22:43:53 +0100

    aaa

--- app/src/test/java/es/unex/parsiapp/model/ColumnaTest.java
+++ app/src/test/java/es/unex/parsiapp/model/ColumnaTest.java
@@ -6,10 +6,6 @@ import org.junit.Test;
 
 public class ColumnaTest {
 
-    public enum ApiCallType {
-        QUERY, USER
-    };
-
     @Test
     public void setIdDb() {
         Columna columna = new Columna();
